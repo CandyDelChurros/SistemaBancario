953185a8798228c0caf505a61725fb1e07b17bdb
CandyDelChurros <cayo.veiga54@gmail.com>
2025-11-28 00:19:57 -0400

    menu de operações

--- app.py
+++ app.py
@@ -6,24 +6,89 @@ from services.database import db
 
 def login_user():
     try:
-        cpf = input("Enter your CPF: ")
+        cpf = input("Entre com o CPF: ")
 
         for client in db["clients"]:
             if client.cpf == cpf:
-                print(f"\nWelcome, {client.name}!\n")
+                print(f"\nBem Vindo, {client.name}!\n")
 
                 for acc in db["accounts"]:
                     if acc.client.cpf == cpf:
                         return acc
 
-                print("Client registered but without an account. Creating account...")
                 result = Account.open_account(db, cpf)
                 print(result["mensagem"])
                 return db["accounts"][-1]
 
-        print("CPF not registered.")
+        print("CPF Não encontrado.")
         return None
 
     except Exception as e:
-        print("Error during login:", e)
+        print("Erro", e)
         return None
+
+def register_user():
+    try:
+        name = input("Nome: ")
+        cpf = input("CPF: ")
+        phone = input("Telefone - Celular: ")
+
+        response = Client.sign_client(db, name, cpf, phone)
+        print(response["mensagem"])
+
+        if response["status"] == "ok":
+            acc_res = Account.open_account(db, cpf)
+            print(acc_res["mensagem"])
+            return db["accounts"][-1]
+
+        return None
+
+    except Exception as e:
+        print("Erro:", e)
+        return None
+
+def operations_menu(account):
+    while True:
+        print(f"Cliente: {account.client.name} | Account: {account.number}")
+        print("1 - Deposito")
+        print("2 - Saque")
+        print("3 - Transferencia")
+        print("4 - Boleto")
+        print("5 - Logout")
+        option = input("Escolha um numero: ")
+
+        try:
+
+            if option == "1":
+                value = float(input("Valor do Deposito: "))
+                r = Transactions.deposit(db, account.number, value)
+                print(r["mensagem"])
+
+            elif option == "2":
+                value = float(input("Valor do Saque: "))
+                r = Transactions.withdraw(db, account.number, value)
+                print(r["mensagem"])
+
+            elif option == "3":
+                to_acc = input("Conta para transferencia: ")
+                value = float(input("Amount: "))
+                r = Transactions.transfer(db, account.number, to_acc, value)
+                print(r["mensagem"])
+
+            elif option == "4":
+                r = Transactions.statement(db, account.number)
+                for k, v in r.items():
+                    print(f"{k}: {v}")
+
+            elif option == "5":
+                break
+
+            else:
+                print("Escolha uma opção certa!")
+
+        except ValueError:
+            print("Numero invalido")
+        except Exception as e:
+            print("Algo de errado aconteceu:", e)
+
+
